d8db72080fa27ef6d3a6403697de836d5c59ca16
Leslie Kerman <kerbal255@outlook.com>
2024-09-02 19:37:53 +0100

    Added fetch message API

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -5,3 +6,19 @@ pub struct SendMessageForm {
     pub content: Box<str>,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    pub content: Box<str>,
+    pub date: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FetchMessagesForm {
+    /// Maximum number of recent messages to fetch.
+    pub max_count: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FetchMessagesResponse {
+    pub messages: Box<[Message]>,
+}
